b9a2be534cc346e441b51e2a55752d2e77681e52
amitvank <amitv0901@gmail.com>
2020-12-20 21:40:42 -0500

    BBG_FEED - Amit Project
    Version1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NewsfeedComponent } from './newsfeed/newsfeed.component';
+import { ContactfeedComponent } from './contactfeed/contactfeed.component';
+import { AboutfeedComponent } from './aboutfeed/aboutfeed.component';
+import { MenufeedComponent } from './menufeed/menufeed.component';
+import { HomeComponent } from './home/home.component';
+import { ConfigComponent } from './config/config.component';
+
+const routes: Routes = [
+  {path:"*",component:HomeComponent},
+  {path:"newsFeed",component:NewsfeedComponent},
+  {path:"contact",component:ContactfeedComponent},
+  {path:"about",component:AboutfeedComponent},
+  {path:"home",component:HomeComponent},
+  {path:"config",component:ConfigComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
